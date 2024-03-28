bd1b23f3c5ea77eb2a0868494b0d99ae00d66bdc
LuiArm <Luisarmendariz24@icloud.com>
2024-03-27 21:13:57 -0600

    Initial Commit

---
+++ Fetch-Rewards/Fetch_RewardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_RewardsApp.swift
+//  Fetch-Rewards
+//
+//  Created by luis armendariz on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_RewardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
